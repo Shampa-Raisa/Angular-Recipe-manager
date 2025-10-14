3235ce06d87a36d83747ed7b2d7131d593f87b04
Shampa-Raisa <shamparaisa15926@gmail.com>
2025-10-14 19:37:26 +0600

    demo of recipe manager

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is simply a test',
+      'https://via.placeholder.com/150',
+      []
+    )
+  ];
+}
